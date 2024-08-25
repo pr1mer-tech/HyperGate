d2fac23408314112d076334c4bb05bbd53b09493
Arthur Guiot <arguiot@gmail.com>
2024-08-25 17:46:33 +0200

    fix: build

--- apps/docs/vocs.config.ts
+++ apps/docs/vocs.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "vocs";
 
-export default defineConfig({
-	rootDir: ".",
-	title: "HyperGate",
-    theme: { 
-        accentColor: { 
-        light: 'black', 
-        dark: 'white', 
-        } 
-    }, 
-    topNav: [ 
-        { text: 'Guide & API', link: '/docs/getting-started', match: '/docs' }, 
-    ],
+const config = defineConfig({
+  rootDir: ".",
+  title: "HyperGate",
+  theme: {
+    accentColor: {
+      light: "black",
+      dark: "white",
+    },
+  },
+  topNav: [
+    { text: "Guide & API", link: "/docs/getting-started", match: "/docs" },
+  ],
 });
+
+export default config;
