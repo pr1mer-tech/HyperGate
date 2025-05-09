db5e0bfa7aa9185478faea725fd9b4924ddb4a9e
Arthur Guiot <arguiot@gmail.com>
2025-05-08 21:17:42 -0700

    fix: hypergate umbrella package

--- packages/hypergate/src/index.ts
+++ packages/hypergate/src/index.ts
@@ -1,15 +1,12 @@
 // Re-export everything from the core package
-export * from '@hyper-gate/core';
-
-// Re-export everything from the react package
-export * from '@hyper-gate/react';
+export * from "@hyper-gate/core";
 
 // Re-export everything from the connectkit package
-export * from '@hyper-gate/connectkit';
+export * from "@hyper-gate/connectkit";
 
 // Export namespaces to allow for clean imports
-import * as Core from '@hyper-gate/core';
-import * as React from '@hyper-gate/react';
-import * as ConnectKit from '@hyper-gate/connectkit';
+import * as Core from "@hyper-gate/core";
+import * as React from "@hyper-gate/react";
+import * as ConnectKit from "@hyper-gate/connectkit";
 
-export { Core, React, ConnectKit }; 
+export { Core, React, ConnectKit };
